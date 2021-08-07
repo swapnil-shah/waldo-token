e37188b91a0df5859f0173d9469dce6c135ec06b
swapnil-shah <swapnilshah.me@gmail.com>
2021-08-07 09:13:47 -0400

    updated roadmap

--- src/sections/roadmap.js
+++ src/sections/roadmap.js
@@ -26,32 +26,33 @@ const Roadmap = () => {
         <div className="flex justify-center">
           <h3 className="subheader">Roadmap</h3>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-4">
-          <div className="col-span-2 relative mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-4 mt-8">
+          <div className="hidden md:block col-span-2">
+            <img
+              className="w-1/2"
+              src="assets/images/waldo_roadmap.png"
+              alt=""
+              style={{ transform: "scaleX(-1)" }}
+            />
+          </div>
+          <div className="col-span-1 md:col-span-2 relative mx-auto md:m-0">
             <ul className="mt-16">
               {items.map((item, index) => (
                 <li
                   key={index}
                   className="roadmap-list border-0 border-dashed border-primary border-l pb-16 pl-8 relative last:border-l-0"
                 >
-                  <h6 className="text-gray-700 text-3xl font-bold tracking-widest -mt-4 mb-2">
+                  <h6 className="text-gray-700 text-3xl font-bold tracking-widest -mt-4 mb-4">
                     {item.title}
                   </h6>
                   {item.list.map((list, i) => (
-                    <p key={i} className="text-gray-500 leading-relaxed pb-2">
+                    <p key={i} className="text-gray-500 leading-relaxed mb-2">
                       {list}
                     </p>
                   ))}
                 </li>
               ))}
             </ul>
-          </div>
-          <div className="col-span-2 mr-2 mx-auto">
-            <img
-              className="w-1/2"
-              src="assets/images/waldo_roadmap.png"
-              alt=""
-            />
           </div>
         </div>
       </div>
